e43ee2913b62d08839ef549d20e24f091a0a6e00
leoribeiro36 <leoribeiro_20@hotmail.com>
2017-05-24 18:33:41 -0300

    adição do projeto node

--- README.md
+++
@@ -1,7 +0,0 @@
-Instalation:
-cmd : npm install inside node_modules/
-cmd : npm install inside node_modules/mongodb
-
-Running:
-call mongod at ~/mongodb/server/3.4/bin
-call node server.js at ~/CasasDoces/
---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function (req, res) {
+
+    var db = require("../db");
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+    var user = new Users({ username: userName, email: userEmail });
+    user.save(function (err) {
+        if (err) {
+            console.log("Error! " + err.message);
+            return err;
+        }
+        else {
+            console.log("Post saved");
+            res.redirect("userlist");
+        }
+    });
+});
+module.exports = router;
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+   var db = require("../db");
+   var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+   Users.find({}).lean().exec(
+      function (e, docs) {
+         res.render('userlist', { "userlist": docs });
+   });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+res.render('newuser', { title: 'Add New User' });
+});
