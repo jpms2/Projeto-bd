b8cb4251c2ef8c29b1ba8d39ebd221f7797a679e
Arthur Freitas <abf2@cin.ufpe.br>
2017-05-25 05:23:33 -0300

    Adiciona novas routes

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var vendedores = require('./routes/vendedores');
+var produtos = require('./routes/produtos');
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/vendedores', vendedores);
+app.use('/produtos', produtos)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -31,37 +31,37 @@ router.get('/produto', function(req, res) {
 res.render('pages/produto', { title: 'Hello, World!' });
 });
 
-/* POST to Add User Service */
-router.post('/adduser', function (req, res) {
+// /* POST to Add User Service */
+// router.post('/adduser', function (req, res) {
 
-    var db = require("../db");
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
+//     var db = require("../db");
+//     var userName = req.body.username;
+//     var userEmail = req.body.useremail;
 
-    var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
-    var user = new Users({ username: userName, email: userEmail });
-    user.save(function (err) {
-        if (err) {
-            console.log("Error! " + err.message);
-            return err;
-        }
-        else {
-            console.log("Post saved");
-            res.redirect("userlist");
-        }
-    });
-});
-module.exports = router;
+//     var Users = db.Mongoose.model(vendedorcollection', db.SellerSchema, 'vendedorcollection');
+//     var user = new Users({ username: userName, email: userEmail });
+//     user.save(function (err) {
+//         if (err) {
+//             console.log("Error! " + err.message);
+//             return err;
+//         }
+//         else {
+//             console.log("Post saved");
+//             res.redirect("userlist");
+//         }
+//     });
+// });
+// module.exports = router;
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-   var db = require("../db");
-   var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
-   Users.find({}).lean().exec(
-      function (e, docs) {
-         res.render('pages/userlist', { "userlist": docs });
-   });
-});
+// /* GET Userlist page. */
+// router.get('/userlist', function(req, res) {
+//    var db = require("../db");
+//    var Users = db.Mongoose.model('vendedorcollection', db.SellerSchema, 'vendedorcollection');
+//    Users.find({}).lean().exec(
+//       function (e, docs) {
+//          res.render('pages/userlist', { "userlist": docs });
+//    });
+// });
 
 /* GET New User page. */
 router.get('/newuser', function(req, res) {
