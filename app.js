0d227ce186ff79a7c679dbacf27cc2285e24c5fb
Arthur Freitas <abf2@cin.ufpe.br>
2017-06-10 20:44:06 -0300

    Fix css and Add Wallpaper image

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/vendedores', vendedores);
 app.use('/produtos', produtos);
