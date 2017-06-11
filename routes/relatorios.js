a98de4540de83dad6c86925f1417ce460799409f
jpms2 <jpms2@cin.ufpe.br>
2017-06-11 03:13:43 -0300

    timeArray now has total bought

--- routes/relatorios.js
+++ routes/relatorios.js
@@ -28,27 +28,26 @@ router.post('/', function(req, res) {
 	}).lean().exec(
 		function (e, docs) {
             if(type == "peaktime"){
-                pricesArray = [0,0,0,0,0]
-                timeArray = [{"time" : "08:00 - 10:00", "qtd" : 0},{"time" : "10:00 - 12:00", "qtd" : 0},{"time" : "12:00 - 14:00", "qtd" : 0},{"time" : "14:00 - 16:00", "qtd" : 0},{"time" : "16:00 - 18:00", "qtd" : 0}];
+                timeArray = [{"time" : "08:00 - 10:00", "qtd" : 0, "totalBought": 0},{"time" : "10:00 - 12:00", "qtd" : 0, "totalBought": 0},{"time" : "12:00 - 14:00", "qtd" : 0, "totalBought": 0},{"time" : "14:00 - 16:00", "qtd" : 0, "totalBought": 0},{"time" : "16:00 - 18:00", "qtd" : 0, "totalBought": 0}];
                 for (var i = 0; docs[i] ; i++) {
                     if (docs[i].data.getHours() < 10){
                         timeArray[0].qtd++;
-                        pricesArray[0] += docs[i].valor
+                        timeArray[0].totalBought += docs[i].valor
                     }if (docs[i].data.getHours() < 12 && docs[i].data.getHours() >= 10){
                         timeArray[1].qtd++;
-                        pricesArray[1] += docs[i].valor
+                        timeArray[1].totalBought += docs[i].valor
                     }if (docs[i].data.getHours() < 14 && docs[i].data.getHours() >= 12){
                         timeArray[2].qtd++;
-                        pricesArray[2] += docs[i].valor
+                        timeArray[2].totalBought += docs[i].valor
                     }if (docs[i].data.getHours() < 16 && docs[i].data.getHours() >= 18){
                         timeArray[3].qtd++;
-                        pricesArray[3] += docs[i].valor
+                        timeArray[3].totalBought += docs[i].valor
                     }if (docs[i].data.getHours() < 18 && docs[i].data.getHours() >= 16){
                         timeArray[4].qtd++;
-                        pricesArray[4] += docs[i].valor
+                        timeArray[4].totalBought += docs[i].valor
                     }
                 }
-                res.render('pages/relatorio', { dataInicial: dataInicial,dataFinal: dataFinal, type: type, array: timeArray, pricesArray: pricesArray});
+                res.render('pages/relatorio', { dataInicial: dataInicial,dataFinal: dataFinal, type: type, array: timeArray});
             }else{
                 res.render('pages/relatorio', { dataInicial: dataInicial,dataFinal: dataFinal, type: type, array: docs});
             }
