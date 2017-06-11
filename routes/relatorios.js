fafd51e1a4ab591066cbff0703bd53002ec1c9d1
jpms2 <jpms2@cin.ufpe.br>
2017-06-11 01:18:11 -0300

    unifying reports route

--- routes/relatorios.js
+++ routes/relatorios.js
@@ -4,9 +4,10 @@ var db = require("../db");
 var Produtos = db.Mongoose.model('produtocollection', db.ProductSchema, 'produtocollection');
 var Pedidos = db.Mongoose.model('pedidocollection', db.PedidoSchema, 'pedidocollection');
 
-router.post('/relatorioHorarioPico', function(req, res) {
+router.post('/relatoriosPedidos', function(req, res) {
 	var dataInicial = req.body.dataInicial;
 	var dataFinal = req.body.dataFinal;
+    var type = req.body.type;
 	var db = require("../db");
 	Pedidos.find({
 		data: {
@@ -15,19 +16,23 @@ router.post('/relatorioHorarioPico', function(req, res) {
     	}
 	}).lean().exec(
 		function (e, docs) {
-			timeArray = [{"time" : "00:00 - 06:00", "qtd" : 0},{"time" : "06:00 - 12:00", "qtd" : 0},{"time" : "12:00 - 18:00", "qtd" : 0},{"time" : "18:00 - 24:00", "qtd" : 0}];
-         for (var i = 0; docs[i] ; i++) {
-         	if (docs[i].data.getHours() < 6){
-                timeArray[0].qtd++;
-            }if (docs[i].data.getHours() < 12 && docs[i].data.getHours() >= 6){
-                timeArray[1].qtd++;
-            }if (docs[i].data.getHours() < 18 && docs[i].data.getHours() >= 12){
-                timeArray[2].qtd++;
-            }if (docs[i].data.getHours() < 24 && docs[i].data.getHours() >= 18){
-                timeArray[3].qtd++;
-                     }
-         }
-			res.render('pages/relatorio', { "pedidos": timeArray });
+            if(type == "peaktime"){
+                timeArray = [{"time" : "00:00 - 06:00", "qtd" : 0},{"time" : "06:00 - 12:00", "qtd" : 0},{"time" : "12:00 - 18:00", "qtd" : 0},{"time" : "18:00 - 24:00", "qtd" : 0}];
+                for (var i = 0; docs[i] ; i++) {
+                    if (docs[i].data.getHours() < 6){
+                        timeArray[0].qtd++;
+                    }if (docs[i].data.getHours() < 12 && docs[i].data.getHours() >= 6){
+                        timeArray[1].qtd++;
+                    }if (docs[i].data.getHours() < 18 && docs[i].data.getHours() >= 12){
+                        timeArray[2].qtd++;
+                    }if (docs[i].data.getHours() < 24 && docs[i].data.getHours() >= 18){
+                        timeArray[3].qtd++;
+                    }
+                }
+                res.render('pages/relatorio', { "dataInicial": dataInicial,"dataFinal": dataFinal, "type": type, "array": timeArray});
+            }else{
+                res.render('pages/relatorio', { "dataInicial": dataInicial,"dataFinal": dataFinal, "type": type, "array": docs});
+            }
 	});
 });
 
